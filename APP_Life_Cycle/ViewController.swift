c85e19279f57b33e9f7fb0d6b51d0bbaf607cf1c
irving926 <irving926@gmail.com>
2016-07-21 19:15:04 +0800

    Initial Commit

---
+++ APP_Life_Cycle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APP_Life_Cycle
+//
+//  Created by 周 修儀 on 2016/7/21.
+//  Copyright © 2016年 周 修儀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
